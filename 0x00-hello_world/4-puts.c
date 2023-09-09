0a3cfd2f8a97d5858cbfbf633a33ae88edf01aa5
Monzer7 <monzer-7@hotmail.com>
2023-09-08 20:16:51 -0700

    this C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,18 +2,12 @@
 /**
  * main - Entry point
  *
- * Return : Always 0 (success)
- *
+ * Return :Always 0 (success)*
  */
 int main(void)
 {
-/**
- * puts - return string
- *
- * Programming is like building a multilingual puzzle
- *
- */
-puts("/"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-	reutrn 0;
-}
+	return (0);
+
+};
