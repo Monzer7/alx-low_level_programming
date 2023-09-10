3c46d1c66a1f69df647f642f3d8806b5bf62394d
Monzer7 <monzer-7@hotmail.com>
2023-09-10 11:18:44 -0700

    thid is numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Discreption: all decimal ssystem digit
+ * Return: Always 0 seccuess
+ */
+int main(void)
+{
+int m;
+for (m = 0 ; m < 9 ; m++)
+printf("%d", m);
+putchar('\n');
+return (0);
+}
