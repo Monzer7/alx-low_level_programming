380dae58bcfb88316a482583e7ba6536bb178c90
Monzer7 <monzer-7@hotmail.com>
2023-09-10 11:28:54 -0700

    thid is numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Discreption: print using char
+ * Return: always 0 success
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i <= 9 ; i++)
+putchar(i + 48);
+putchar('\n');
+return (0);
+}
