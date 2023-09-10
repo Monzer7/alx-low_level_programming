f9b3a2c6797bc48181decc4620edc083b140ac0f
Monzer7 <monzer-7@hotmail.com>
2023-09-10 10:12:31 -0700

    this ic C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Discreption: a function to print the alphapete
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char M;
+for (M = 'a'; M >= 'z'; M++)
+putchar(M);
+return (0);
+}
