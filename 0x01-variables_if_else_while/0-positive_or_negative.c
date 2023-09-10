cba31aa046abd82566f705d2787814179f1dc9de
Monzer7 <monzer-7@hotmail.com>
2023-09-09 18:36:30 -0700

    this logic in c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Discreption: this function show you neg and pos
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+
+int n;
+
+if (n > 0)
+{
+printf("%d is positive, n\n");
+}
+
+else if (n == 0)
+{
+printf("%d is zero, n\n");
+}
+
+else(n < 0)
+{
+printf("%d is negative, n\n");
+};
+
+return (0);
+}
