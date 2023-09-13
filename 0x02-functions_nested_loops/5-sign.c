106766e90d64c4e47f08fb56e44d57bf98a58914
Monzer7 <monzer-7@hotmail.com>
2023-09-13 07:00:45 -0700

    sdc

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - print char sign
+ *
+ * @n : entry by user
+ *Return: 0 if zero , -1 if neg and +1 if pos
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+} else if (n == 0)
+{
+_putchar(48);
+return (0);
+} else if (n < 0)
+{
+_putchar('-');
+}
+return (-1);
+}
