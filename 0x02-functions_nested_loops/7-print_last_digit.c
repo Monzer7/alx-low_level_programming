17cb148cc6fe5fc034fcb461b9347abdb737ec2a
Monzer7 <monzer-7@hotmail.com>
2023-09-13 08:45:58 -0700

    ds

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,13 +3,21 @@
  * print_last_digit - prints the last digit of a number
  * @n: the int to extract the last digit from
  * Return: value of the last digit
+ *
  */
 int print_last_digit(int n)
 {
-int m = 0;
+int a;
+
 if (n < 0)
-m = -n % 10;
-else
-_putchar(m + '0');
-return (m);
+n = -n;
+
+a = n % 10;
+
+if (a < 0)
+a = -a;
+
+_putchar(a + '0');
+
+return (a);
 }
