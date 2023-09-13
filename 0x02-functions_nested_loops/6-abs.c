cc9912a0ae697fbbc088e889c59f51ccf723d7af
Monzer7 <monzer-7@hotmail.com>
2023-09-13 07:41:07 -0700

    fdc

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * _abs(int m): computes the absolute value of an integer
+ * _abs -  computes the absolute value of an integer
+ * Description: absolute value
  * @m: the int to check
  * Return: the absolute value of int
  */
 int _abs(int m)
 {
 if (m < 0)
-return (m);
+return (-m);
 else
 return (m);
 }
