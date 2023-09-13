561169a9a3e1434419051aa2386bde1511356da2
Monzer7 <monzer-7@hotmail.com>
2023-09-12 17:59:37 -0700

    nested

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - entry point
  *
  * Description: this function print putchar
- * return: Always 0 success
+ * Return: Always 0 success
  */
 int main(void)
 {
-string h = "_putchar";
-printf("%s",h);
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
