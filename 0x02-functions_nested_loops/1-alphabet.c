ffb22fc8b46755c710f3a6ce1ce20bbdb231263b
Monzer7 <monzer-7@hotmail.com>
2023-09-12 18:23:40 -0700

    dd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * description: print alphabet in lowwr case
+ * Return: always (0)
+ */
+int main(void)
+{
+char  m;
+for (m = 97 ; m <= 122 ; m++)
+_putchar(m);
+_putchar(11);
+return (0);
+}
