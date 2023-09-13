2600b075f9a28d1f7a86d3992e8170d5522bd1cd
Monzer7 <monzer-7@hotmail.com>
2023-09-12 17:37:34 -0700

    nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: this function print putchar
+ * return: Always 0 success
+ */
+int main(void)
+{
+string h = "_putchar";
+printf("%s",h);
+return (0);
+}
