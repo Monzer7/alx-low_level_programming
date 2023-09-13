4d2e188acb8c73c340863777e9c4874250b0b94e
Monzer7 <monzer-7@hotmail.com>
2023-09-12 18:33:50 -0700

    sd

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,11 @@
  * description: print alphabet in lowwr case
  * Return: always (0)
  */
-int main(void)
+void print_alphabet(void)
 {
 char  m;
 for (m = 97 ; m <= 122 ; m++)
 _putchar(m);
 _putchar(11);
-return (0);
+return ;
 }
