7a69a93d70f74d2606e0da419ba51f4dbbd4fec4
Monzer7 <monzer-7@hotmail.com>
2023-09-20 01:09:52 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * changine the value of any intger to 98
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+int p;
+p = 98;
+n = &p;
+return;
+}
