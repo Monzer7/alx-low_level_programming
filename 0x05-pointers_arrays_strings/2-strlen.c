31d3fc1dbecde15b5ac968bd40288dee3186512c
Monzer7 <monzer-7@hotmail.com>
2023-09-20 02:08:00 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - calculate the str length
+ * Description: IT counts char on by one
+ *
+ * @s: is the string pointer
+ * Return:  Always int number of char per str
+ */
+int _strlen(char *s)
+{
+int m;
+m = *s - 1;
+return (m);
+}
