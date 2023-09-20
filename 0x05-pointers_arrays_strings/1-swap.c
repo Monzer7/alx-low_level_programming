8b2f62f696ae236f361d193e25db2e1529dc2863
Monzer7 <monzer-7@hotmail.com>
2023-09-20 01:50:15 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * swap_int - swap to var values
+ * Description: it passes the value of the first
+ * var into the value of the second and vise versa
+ *
+ * @a: first var of swapping
+ * @b: second var of swapping
+ */
+void swap_int(int *a, int *b)
+{
+int m = 0;
+*a = m;
+*a = *b;
+*b = m;
+return;
+}
