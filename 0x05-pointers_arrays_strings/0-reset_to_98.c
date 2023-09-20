659367d0128d8f8b42cf6db34837064b0dc664ec
Monzer7 <monzer-7@hotmail.com>
2023-09-20 01:33:50 -0700

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * void reset_to_98 - takes the inter int and
- * passes the value of 98 to ut
+ * reset_to_98 - pass the value of 98
+ * description: IT passes the value of 98 to the var
  *
- * @*n is the parameter the passes the value to argument
+ * @n: is the parameter the passes the value to argument
  * Return: Always 0.
  */
 void reset_to_98(int *n)
