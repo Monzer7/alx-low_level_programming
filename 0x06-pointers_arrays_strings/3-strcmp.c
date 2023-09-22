678f081d6e7099e69c8d20d5c9711cca3693c075
Monzer7 <monzer-7@hotmail.com>
2023-09-22 05:11:36 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcmp - function compare twp string
+ * @s1: one string
+ * @s2: one string
+ * Return: int that tells num spaces in between
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+while (*(s1 + i) == *(s2 + i) && *(s1 + i))
+i++;		
+return (*(s1 + i) - *(s2 + i));
+}
