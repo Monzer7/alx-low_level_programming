ce02c6bc367d1dde53f270804eba4bdc37a1d8f9
Monzer7 <monzer-7@hotmail.com>
2023-09-22 07:21:23 -0700

    arrays

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * string_toupper - capitalize all letters in string
+ *
+ * @s:  for manipulate string values
+ * Return: string with all letters capitalized
+ */
+char *string_toupper(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+if (s[i] > 97 && s[i] < 122)
+s[i] = s[i] - 97 + 65;
+return (s);
+}
