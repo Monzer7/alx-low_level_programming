7a97c8c6946e44aa79a6d0666a6e4bb0b171efd3
Monzer7 <monzer-7@hotmail.com>
2023-09-22 14:14:02 -0700

    pointers

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,15 +7,20 @@
  */
 char *rot13(char *s)
 {
-int i = 0;
-while (s[i] != '\0')
+int i;
+int j; /* temp*/
+char e [54] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char c [54] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+for (j = 0; s[j] != '\0'; j++) /*loop through string*/
 {
-for (i = 0; i >= 'a' && i <= 'z'; i++)
-if (i <= 'm')
-s[i] = s[i + 13];
-else if (i >= 'n')
-s[i] = s[i - 13];
+i = 0;
+while (e[i] != '\0' && s[j] != e[i]) /*loop through rot13 arr*/
 i++;
+
+if (s[j] == e[i]) /*if alpha matches, set to index in b arr*/
+s[j] = c[i];
 }
+
 return (s);
 }
