a60e57d9f34064e84325cf00a672e0f42ad2069d
Monzer7 <monzer-7@hotmail.com>
2023-09-22 07:33:48 -0700

    pointers

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,13 +4,25 @@
  *
  * @s:  for manipulate string values
  * Return: string with all letters capitalized
+ *
+ *char *string_toupper(char *s)
+ *{
+ *int i;
+ *for (i = 0; s[i] != '\0'; i++)
+ *{
+ *if (s[i] > 'a' && s[i] < 'z')
+ *s[i] = s[i] - 'a' + 'A';
+ *}
+ *return (s);
+ *}
  */
 char *string_toupper(char *s)
 {
-int i;
+
+int i = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
-if (s[i] > 'a' && s[i] < 'z')
+if (s[i] >= 'a' && s[i] <= 'z')
 s[i] = s[i] - 'a' + 'A';
 }
 return (s);
