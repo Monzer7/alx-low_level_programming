8aa5094c1ca8980a8fa3bae227dea56aa337ad8a
Monzer7 <monzer-7@hotmail.com>
2023-09-22 06:50:08 -0700

    pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,14 +8,14 @@
 void reverse_array(int *a, int n)
 {
 
-int i;
-int j;
-int m = n;
+int i = 0;/* var for *a */
+int j;/* temperorary var for saving vars*/
+int m = n - 1;/* var for array size*/
 while (i < m)
 {
 j = *(a + i);
 *(a + i) = *(a + m);
 *(a + m) = j;
-i++, j--;
+i++, m--;
 	}
 }
