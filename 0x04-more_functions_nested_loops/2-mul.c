91b2dbb8e2da3e776f00001111901d8803db3c1d
Monzer7 <monzer-7@hotmail.com>
2023-09-24 07:42:31 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplt two numbers
+ * @a: first number
+ * @b: second number
+ * @res: multiblication result
+ * Return: multiplication result
+ */
+int mul(int a, int b)
+{
+int res;
+res = a * b;
+return (res);
+}
