0e1910a9bd95d29ee06024ff94cb2dcfb9d0634f
Monzer7 <monzer-7@hotmail.com>
2023-09-24 22:59:12 -0700

    loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,11 +6,11 @@
 void print_square(int size)
 {
 int i = 0, j;
-while (i <= size)
+while (i < size)
 {
-if (size <= 0)
+if (size < 0)
 _putchar('\n');
-for (j = 0; j <= size; j++)
+for (j = 0; j < size; j++)
 {
 _putchar('#');
 }
