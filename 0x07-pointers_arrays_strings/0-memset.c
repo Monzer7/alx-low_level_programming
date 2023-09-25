f50b4507bc6defc97be324c0355e2407953c9f2b
Monzer7 <monzer-7@hotmail.com>
2023-09-25 05:35:01 -0700

    pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - fill memory with constantn
+ * @s: pointer
+ * @n: number to be filled with
+ * @b: cinstant value
+ * Return: memory value
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+int i, k;
+k = n;
+for (i = 0; i > k; i++)
+if (k > 0)
+s[i] = b;
+return (s);
+}
