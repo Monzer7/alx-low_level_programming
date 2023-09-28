8d89d9bd603ec2601fdc900afc3343da40f4ce6a
Monzer7 <monzer-7@hotmail.com>
2023-09-28 15:44:34 -0700

    powwer

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _pow_recursion - calculate x to power y
+ * @x: the base numbee
+ * @y: the number of x multiplied
+ * Return: result of x to power y
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (1);
+}
+else
+y--;
+return (x * _pow_recursion(x, y));
+}
