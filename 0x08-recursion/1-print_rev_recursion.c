f4958b153b39a864517bbbc66d466eea1fa2fe20
Monzer7 <monzer-7@hotmail.com>
2023-09-28 14:31:27 -0700

    recursion is fun /

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: string to be reversed
+ * return: always 0
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+return;
+}
+else
+s++;
+_print_rev_recursion(s);
+_putchar(*s);
+}
