4d41b214309934e6458a153af9b614fccc8b9071
Monzer7 <monzer-7@hotmail.com>
2023-10-02 14:07:40 -0700

    statis

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+ char *_strncat(char *dest, char *src, int n)
+{
+return _strncat(dest, src, n);
+}
+
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h> 
+char *_strcpy(char *desr, char *src)
+{
+return (_strcpy(desr, src));
+}
+
