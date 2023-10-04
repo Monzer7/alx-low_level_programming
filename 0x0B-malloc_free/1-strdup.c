7c9791a7e109d531ece27310501e2e8cbcf488d7
Monzer7 <monzer-7@hotmail.com>
2023-10-04 02:51:55 -0700

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * lenght - measure the lenght of the string
+ * @str: string to be measured
+ * Return: the lenght of the string
+ */
+int lenght(char *str)
+{
+int i, l;
+for (i = 0; str[i] != '\0'; i++)
+l = i;
+return (l);
+}
+
+
+/**
+ * *_strdup - create a pointer to string with conataint
+ * of another string
+ * @str: string to be copied
+ * Return: a pointer to the string or NULL
+ */
+char *_strdup(char *str)
+{
+char *m;
+int k;
+if (str == NULL)
+return (NULL);
+m = malloc(sizeof(char) * lenght(str));
+k = 0;
+while (str[k] != '\0')
+{
+m[k] = str[k];
+k++;
+}
+return (m);
+}
