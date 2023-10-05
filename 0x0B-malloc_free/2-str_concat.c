3531b134584d6b9151dc5be9d24d314df6847b90
Monzer7 <monzer-7@hotmail.com>
2023-10-05 01:15:59 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,26 +10,25 @@
 char *str_concat(char *s1, char *s2)
 {
 char *m;
-int len1 = 0, len2 = 0, i = 0, j = 0, k = 0;
+int len1 = 0, len2 = 0, i = 0, j = 0, k = 0, f = 0;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";	
 while (*(s1 + i))
-{
-i++, len1++;
-}
+{i++, len1++;}
 while (*(s2 + j))
-{
-j++; len2++;
-}
+{j++, len2++;}
 m = malloc(sizeof(char) * (len1 + len2));
 if (m == NULL)
+
 return (NULL);
 while (k < len1)
 {
 *(m + k) = *(s1 + k);
+k++;
+}
+while (f < (len1 + len2))
+{
+*(m + len1 + f) = *(s2 + f);
+f++;
 }
 return (m);
 }
