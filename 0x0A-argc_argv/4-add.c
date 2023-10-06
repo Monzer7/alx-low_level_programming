eeb1b878aaa552daed768c54e6df1170e5ba5f4e
Monzer7 <monzer-7@hotmail.com>
2023-10-06 15:18:09 -0700

    sdfs

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,6 +13,7 @@ int j, res = 0;
 if (argc == 1)
 {
 printf("0\n");
+return (0);
 }
 for (j = 1; j < argc; j++)
 {
@@ -23,7 +24,7 @@ else
 printf("Error\n");
 return (1);
 }
-printf("%d\n", res);
 }
+printf("%d\n", res);
 return (0);
 }
