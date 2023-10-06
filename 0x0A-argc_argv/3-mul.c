490ea7b4ca2f98a3d2b0b8f7348600340ff03a26
Monzer7 <monzer-7@hotmail.com>
2023-10-06 15:07:14 -0700

    dfs

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,18 +9,17 @@
 int main(int argc, char **argv)
 {
 int res;
-int num1;
-int num2;
 
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-res = num1 * num2;
+else
+{
+res = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n", res);
+}
 return (0);
 }
 
