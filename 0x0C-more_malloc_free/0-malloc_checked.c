97a6eed069c32fd4cf521da40b621097be80dea3
Monzer7 <monzer-7@hotmail.com>
2023-10-08 23:56:44 -0700

    poiter

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *malloc_checked - allocates memory dynamicaly
+ * @b: number of allociated blocks
+ * Return: pointer to the memory allocated
+ */
+void *malloc_checked(unsigned int b)
+{
+int *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
+printf("98");
+return (ptr);
+}
+
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+more malloce excercise
