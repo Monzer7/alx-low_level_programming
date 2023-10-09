7473738cac1239fa7fe39c112bb2a7718de627fd
Monzer7 <monzer-7@hotmail.com>
2023-10-09 14:41:31 -0700

    sd

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -35,7 +35,7 @@ p = malloc(new_size);
 if (p == NULL)
 return (NULL);
 
-for (i = 0; i < new_size; i++)
+for (i = 0; i < old_size && i < new_size; i++)
 *((char *)p + i) = *((char *)ptr + i);
 free(ptr);
 
