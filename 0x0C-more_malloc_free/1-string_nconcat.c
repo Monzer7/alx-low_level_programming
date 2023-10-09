167e48286b9cbea571fa333169f4c4e39f5c743c
Monzer7 <monzer-7@hotmail.com>
2023-10-09 02:43:55 -0700

    Cdscxz

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,14 +7,13 @@
  * @s:string to be measured
  * Return: string lenght
  */
-int len(char *s)
+int _strlen(char *string)
 {
-int m, l;
-for (m = 0; s[m] != '\0'; m++)
-{
-l = m;
-}
-return (l);
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+		;
+	return (i);
 }
 /**
  *  *string_nconcat - function concatenate two strings
@@ -64,7 +63,7 @@ return (ptr);
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	int num, lenght, i, j;
+	int num, len, i, j;
 
 	num = n;
 
@@ -74,12 +73,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	if (num < 0) /* account for negative n bytes */
 		return (NULL);
-	if (num >= len(s2)) /* account for n too big */
-		num = len(s2);
+	if (num >= _strlen(s2)) /* account for n too big */
+		num = _strlen(s2);
 
-	lenght = len(s1) + num + 1; /* +1 to account for null pointer */
+	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
 
-	ptr = malloc(sizeof(*ptr) * lenght); /* malloc and check for error */
+	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
 	if (ptr == NULL)
 		return (NULL);
 
