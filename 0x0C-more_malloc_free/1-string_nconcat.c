79ff665ea6a0f83cc760b469d69b36a789c42ddd
Monzer7 <monzer-7@hotmail.com>
2023-10-09 02:51:48 -0700

    Cdscxz

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,23 +27,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 char *ptr;
 int i, j, lenght;
 int k = n;
-if (k >= _strlen(s2))
-{
-k = _strlen(s2);
-}
-if (k < 0)
-{
-return (NULL);
-}
+
 if (s1 == NULL)
-{
 s1 = "";
-}
+
 if (s2 == NULL)
-{
 s2 = "";
-}
-lenght = _strlen(s1) + _strlen(s2);
+
+if (k < 0)
+return (NULL);
+
+if (k >= _strlen(s2))
+k = _strlen(s2);
+
+lenght = _strlen(s1) + k;
 
 ptr = malloc(sizeof(*ptr) * lenght);
 if (ptr == NULL)
