1c504617a691bce45c014592c323ef7a3d09dcbe
Monzer7 <monzer-7@hotmail.com>
2023-10-09 03:02:25 -0700

    Cdscxz

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,17 +3,16 @@
 #include <string.h>
 #include <stdlib.h>
 /**
- * len - calculate str lenght
- * @s:string to be measured
+ * _strlen - calculate str lenght
+ * @string:string to be measured
  * Return: string lenght
  */
 int _strlen(char *string)
 {
-	int i;
-
-	for (i = 0; string[i] != '\0'; i++)
-		;
-	return (i);
+int i;
+for (i = 0; string[i] != '\0'; i++)
+;
+return (i);
 }
 /**
  *  *string_nconcat - function concatenate two strings
@@ -56,33 +55,3 @@ ptr[i + j] = s2[j];
 ptr[i + j] = '\0';
 return (ptr);
 }
-
-/**char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	char *ptr;
-	int num, len, i, j;
-
-	num = n;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	if (num < 0) 
-		return (NULL);
-	if (num >= _strlen(s2)) 
-		num = _strlen(s2);
-
-	len = _strlen(s1) + num + 1; 
-
-	ptr = malloc(sizeof(*ptr) * len); 
-	if (ptr == NULL)
-		return (NULL);
-
-	for (i = 0; s1[i] != '\0'; i++) 
-		ptr[i] = s1[i];
-	for (j = 0; j < num; j++)
-		ptr[i + j] = s2[j];
-	ptr[i + j] = '\0';
-
-	return (ptr);}*/
