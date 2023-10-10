b3723a2feafcfdc4d2a5966602d47bcd9d398435
Monzer7 <monzer-7@hotmail.com>
2023-10-10 15:11:47 -0700

    sdds

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,12 +15,12 @@ char *owner;
 };
 
 /**
- * u_int - Typedef for unsigned int
+ * dog_t -  Typedef for struct dog
  */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
