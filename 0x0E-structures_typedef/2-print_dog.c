20025c6308f313881f1d9e28882cb7245e3c0568
Monzer7 <monzer-7@hotmail.com>
2023-10-10 15:34:28 -0700

    sdds

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints dog contains
+ * @d: name of file
+ * Return: no return
+ */
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+return;
+
+else if (d->name == NULL)
+printf("Name: (nil)\n");
+
+
+else if (d->owner == NULL)
+printf("Owner: (nil)\n");
+
+else
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
