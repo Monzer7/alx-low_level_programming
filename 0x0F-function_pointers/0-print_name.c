2dd84ec5947354ae19e27117a4f7e2183a486348
Monzer7 <monzer-7@hotmail.com>
2023-10-11 16:10:19 -0700

    fesd

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,18 +2,6 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 /**
- * print_name - prints a name
- * @name: name tobe printed
- * @f: function to be called back to be excetued
- * Return: no return
- */
-/*void print_name(char *name, void (*f)(char *))
-{
-if (name != NULL && f != NULL)
-f(name);
-}
-#include <stdlib.h>
-
  * print_name - execute a function given as a parameter
  * @name: parameter that function ptr needs
  * @f:function to execute
