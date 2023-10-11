9062d8ee04bfca42347a88cce582468afb9c2d3d
Monzer7 <monzer-7@hotmail.com>
2023-10-11 14:45:36 -0700

    fsvcs

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,16 +5,10 @@
  * print_name - prints a name
  * @name: name tobe printed
  * @f: function to be called back to be excetued
- * Return: no return 
+ * Return: no return
  */
-/**void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 if (name != NULL && f != NULL)
 f(name);
-}*/
-
-void print_name(char *name, void (*f)(char *))
-{
-	if (name != NULL && f != NULL)
-		f(name);
 }
