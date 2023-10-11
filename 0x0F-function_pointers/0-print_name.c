e3b47ea183d305c3832fac92330910ee9b5cca8d
Monzer7 <monzer-7@hotmail.com>
2023-10-11 14:43:49 -0700

    fsvcs

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,8 +7,14 @@
  * @f: function to be called back to be excetued
  * Return: no return 
  */
-void print_name(char *name, void (*f)(char *))
+/**void print_name(char *name, void (*f)(char *))
 {
 if (name != NULL && f != NULL)
 f(name);
+}*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
 }
