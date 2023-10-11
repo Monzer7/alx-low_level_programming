5bee095ed27c05c7440a6e0fe8f7da0799499a9f
Monzer7 <monzer-7@hotmail.com>
2023-10-11 02:39:31 -0700

    sd

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: name tobe printed
+ * @f: function to be called back to be excetued
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+return;
+else
+f(name);
+}
