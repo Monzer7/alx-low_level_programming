077324334854b186ba6268c13d242a6729fc5a58
Monzer7 <monzer-7@hotmail.com>
2023-10-12 14:19:53 -0700

    sds

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,7 +7,7 @@
  * @n: number of strings passed
  * return: no return
  */
-void print_strings(const char *separator, const unsigned int n, ...)
+/**void print_strings(const char *separator, const unsigned int n, ...)
 {
 va_list monzer;
 unsigned int i;
@@ -29,4 +29,28 @@ printf("%s", separator);
 va_end(monzer);
 }
 printf("\n");
+}*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+	char *s;
+
+	if (n > 0)
+	{
+		va_start(valist, n);
+		for (i = 1; i <= n; i++)
+		{
+			s = va_arg(valist, char *);
+			if (s == NULL)
+				printf("(nil)");
+			else
+				printf("%s", s);
+
+			if (i != n && separator != NULL)
+				printf("%s", separator);
+		}
+		va_end(valist);
+	}
+	printf("\n");
 }
