f44f354a3e87a75e3df79a89b0263b0916c7ef9a
Monzer7 <monzer-7@hotmail.com>
2023-10-12 02:11:05 -0700

    sd

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - prints passed numbers
+ * @separator: char between numbers
+ * @n: number of nmber to be printed
+ * Return: no return
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list monzer;
+unsigned int i;
+
+if (n != 0)
+{
+va_start(monzer, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(monzer, int));
+if (separator != NULL)
+printf("%s", separator);
+}
+va_end(monzer);
+}
+printf("\n");
+}
