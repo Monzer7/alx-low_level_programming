5382a37032e2ed13e796d0bbe8400ddf4f12e311
Monzer7 <monzer-7@hotmail.com>
2023-10-23 01:43:22 -0700

    modify counter

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 /**
  *print_listint - prints all list elemnts
  *@h: list to be printed
@@ -6,12 +6,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t num;
+size_t num = 0;
 while (h != NULL)
 {
 printf("%d\n", h->n);
-num++
-h = h->next
+num++;
+h = h->next;
 }
 return (num);
 }
