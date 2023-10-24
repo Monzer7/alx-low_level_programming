144477c399820619b557ed56bf7466698b5294fb
Monzer7 <monzer-7@hotmail.com>
2023-10-24 00:41:53 -0700

    sd

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *free_listint2 - free lists
+ *@head: adress of list
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *p;
+if (*head == NULL)
+	return;
+
+while (*head != NULL)
+{
+p = *head;
+free(p);
+*head = *head->next;
+}
+*head == NULL;
+}
