2ab70c624bc89dcf679c9186d4993a6868e4483e
Monzer7 <monzer-7@hotmail.com>
2023-10-24 02:17:02 -0700

    sfs

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - function that search that search a list
+ * @head: adress of the list
+ * @index: location of nore to be returnr
+ * Return: adrees of nth node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int i;
+listint_t *new;
+if (head == NULL)
+	return (NULL);
+
+for (i = 0; i < index; i++)
+{
+new = head;
+new = new->next;
+}
+return (new);
+}
