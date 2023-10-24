a6f1923aa6731720b5e37b70c466d08b31c5e288
Monzer7 <monzer-7@hotmail.com>
2023-10-24 00:51:26 -0700

    sd

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,16 +5,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *ptr;
+listint_t *ptr;
 
 if (head == NULL)
 	return;
 
-	while (*head != NULL) 
+while (*head != NULL)
 {
-	ptr = *head;
-	*head = ptr->next;
-	free(ptr);
-	}
-
+ptr = *head;
+*head = ptr->next;
+free(ptr);
+}
 }
