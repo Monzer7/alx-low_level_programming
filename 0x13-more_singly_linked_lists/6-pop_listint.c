50ce33dbd179d1f64c72f1b4e2354354d02d7fb0
Monzer7 <monzer-7@hotmail.com>
2023-10-24 01:38:58 -0700

    pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * pop_listint - delete the head of the node
+ * @head: adrees of the node;
+ * Return: the value in head node
+ */
+int pop_listint(listint_t **head)
+{
+int value;
+listint_t *new;
+new = *head;
+if (head == NULL)
+	return (0);
+else
+{
+value = new->n;
+*head = new->next;
+}
+return (value);
+}
