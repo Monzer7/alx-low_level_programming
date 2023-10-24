88c9d32884d47d923b88b4e46568cd547e6ba341
Monzer7 <monzer-7@hotmail.com>
2023-10-24 00:19:32 -0700

    clearing error

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,6 +6,14 @@
  */
 void free_listint(listint_t *head)
 {
-free(head->next);
-free(head);
+	if (head == NULL)
+		return;
+
+	listint *rm;
+while (head != NULL)
+{	
+	rm = head;
+	free(rm);
+head= head->next;
+}
 }
