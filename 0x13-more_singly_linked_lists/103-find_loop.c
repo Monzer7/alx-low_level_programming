149e5c66320682659058a0ea7f5b4f3c8ea6a181
Monzer7 <monzer-7@hotmail.com>
2023-10-25 01:38:48 -0700

    fdd

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ *find_listint_loop - finds aloop in linked lists
+ *@head: adress of list
+ *Return: address of the head NULL in failure
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+listint_t *h, *t;
+h = t = head;
+while (h != NULL && t != NULL)
+{
+	h = h->next;
+	t = t->next->next;
+	if (h == t)
+		h = head;
+	while (h != t)
+	{
+		h = h->next;
+		t = t->next;
+	}
+return (h);
+}
+return (NULL);
+}
