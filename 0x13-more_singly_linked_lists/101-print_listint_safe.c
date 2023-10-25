97984ef0a73e687da42bfc3534909194b91e51d5
Monzer7 <monzer-7@hotmail.com>
2023-10-25 01:00:07 -0700

    sds

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ *print_listint_safe - prints list
+ *@head: list to be printed address
+ *Return: number of nodes & 98 in failure
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+int count;
+const listint_t *new;
+if (head == NULL)
+	return (98);
+
+new = head;
+
+while (new->next != NULL)
+{
+	printf("[%p] %d\n",(void *)new, new->n);
+		new = new->next;
+	count++;
+
+}
+return (count);
+}
