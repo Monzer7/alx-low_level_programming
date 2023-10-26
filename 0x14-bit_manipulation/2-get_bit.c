7cc7fe4d875332a90ce85c2f3ed5d0ecb879917e
Monzer7 <monzer-7@hotmail.com>
2023-10-26 15:26:00 -0700

    git index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *get_bit - find bit at given index
+ *@n: number to be searchrd
+ *@index: number's location
+ *Return: in success 1 or 0 in failure -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+int bit;
+unsigned int maxlen;
+naxlen = (sizeof(unsigned long int) * 8);
+if (index > maxlen)
+	return (-1);
+bit = ((n >> index) & 1);
+return (bit);
+}
