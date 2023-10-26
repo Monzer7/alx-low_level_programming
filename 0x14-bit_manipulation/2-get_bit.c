c6624ef96af7b4e20e55b6509781bb87bc549e4e
Monzer7 <monzer-7@hotmail.com>
2023-10-26 15:28:03 -0700

    git bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,7 +9,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 int bit;
 unsigned int maxlen;
-naxlen = (sizeof(unsigned long int) * 8);
+maxlen = (sizeof(unsigned long int) * 8);
 if (index > maxlen)
 	return (-1);
 bit = ((n >> index) & 1);
