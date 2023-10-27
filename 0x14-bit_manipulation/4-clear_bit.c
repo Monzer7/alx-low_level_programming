1742dd7aacc1479121cfa68ccb4ef313ca220217
Monzer7 <monzer-7@hotmail.com>
2023-10-26 19:47:44 -0700

    clear

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - clear bit to 0 at given index
+ * @n: number
+ * @index: index within binary number
+ * Return: 1 if success, or -1 if error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int maxlen;
+	unsigned long int bit = 1;
+
+	maxlen = (sizeof(unsigned long int) * 8);
+	if (index > maxlen)
+		return (-1);
+
+	bit = ~(bit << index);
+	*n = (*n & bit);
+
+	return (1);
+}
