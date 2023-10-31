8a60946e9bfd8e8fbfa08cd51b4e721f02039e32
Monzer7 <monzer-7@hotmail.com>
2023-10-31 02:45:55 -0700

    editing

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,75 +1,39 @@
 #include "main.h"
 /**
- * read_textfile - function read text file
+ * read_textfile - function read from file and write to stdo
  * @filename: file to be read from
  * @letters: number of letter to be read
- * Return: the number of letter read.
- */
-/*ssize_t read_textfile(const char *filename, size_t letters)
-{
-	int fp;
-	ssize_t nread, nwrite;
-	char *file;
-
-	if (filename == NULL)
-		return (0);
-	fp = open("filename", O_RDONLY);
-
-	if (fp == -1)
-		return (0);
-	file = malloc(sizeof(char) * letters);
-	if (file == NULL)
-		return (0);
-	nread = read(fp, file, letters);
-	if (nread == -1)
-	{
-		free(file);
-		close(fp);
-		return (0);
-	}
-	nwrite = write(STDOUT_FILENO, file, nread);
-		if (nwrite == -1)
-		{
-			free(file);
-	close(fp);
-	return (0);
-		}
-	close(fp);
-	return (nwrite);
-}*/
-#include "main.h"
-
-/**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
- *
- * Return: numbers of letters printed. It fails, returns 0.
+ * Return: the number of letter read and prints
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+ssize_t nread, nwrite;
+int p;
+char *buf;
 
-	if (!filename)
-		return (0);
+/*creating buufer*/
 
-	fd = open(filename, O_RDONLY);
+buf = malloc(sizeof(char) * letters);
+if (buf == NULL)
+	return (0);
 
-	if (fd == -1)
-		return (0);
-
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
-		return (0);
-
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
-	close(fd);
-
-	free(buf);
-
-	return (nwr);
+if (filename == NULL)
+	return (0);
+/*opening file*/
+p = open(filename, O_RDONLY);
+if (p == -1)
+	return (0);
+/*reading file*/
+nread = read(p, buf, letters);
+if (nread == -1)
+	return (0);
+/*write to stndard output*/
+nwrite = write(STDOUT_FILENO, buf, nread);
+if (nwrite == -1)
+	return (0);
+/* free buf*/
+free(buf);
+/*closing file*/
+close(p);
+return (nread);
 }
