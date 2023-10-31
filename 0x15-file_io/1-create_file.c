47f6d51a5f2d052dc8c88a5cffa6304bf21b9d45
Monzer7 <monzer-7@hotmail.com>
2023-10-31 03:23:03 -0700

    cdcz

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *create_file - it creates a file
+ *@filename: the name of the file to be created
+ *@text_content: content to ba added to the file
+ *Return: on seccuess 1 ,on failure -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+int p;
+int trunc, lenght;
+
+if (filename == NULL)
+	return (-1);
+/*creating file*/
+
+p = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0666);
+if (p == -1)
+	return (-1);
+if (text_content == NULL)
+text_content = "";
+/*trunecate if found*/
+
+for (lenght = 0; text_content[lenght] != '\0'; lenght++)
+;
+
+trunc = write(p, text_content, lenght);
+if (trunc == -1)
+	return (-1);
+/*closing */
+close(p);
+return (1);
+}
