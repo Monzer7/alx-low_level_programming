135acd53356d2c36dfac87df47e161bbe1b41445
Monzer7 <monzer-7@hotmail.com>
2023-10-31 15:03:34 -0700

    vccopp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -63,5 +63,5 @@ if (file_from == -1)
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 
 if (file_to == -1)
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 }
