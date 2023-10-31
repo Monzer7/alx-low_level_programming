f7426122158a4c19f4970cbd5db24dc12be9ddeb
Monzer7 <monzer-7@hotmail.com>
2023-10-31 00:07:04 -0700

    fds

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,7 @@
  * @letters: number of letter to be read
  * Return: the number of letter read.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+/*ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fp;
 	ssize_t nread, nwrite;
@@ -35,5 +35,41 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	return (0);
 		}
 	close(fp);
-	return (nread);
+	return (nwrite);
+}*/
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
 }
