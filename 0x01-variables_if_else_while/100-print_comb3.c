748188b90a458d4b749cdb208d26933cb6f6bade
Monzer7 <monzer-7@hotmail.com>
2023-11-22 04:11:52 -0800

    print tomp

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,17 @@ int main(void)
 int m;
 int n;
 for (m = 0 ; m < 9 ; m++)
-for (n = m + 1 ; n < 10 ; n++)
-putchar(m + 48);
-putchar(n + 48);
+{
+for (n = 0 ; n < 10 ; n++)
+{
+putchar(m);
+putchar(n);
 if (m == 8 && n == 9)
 {
 putchar(',');
 putchar(' ');
+}
+}
 }
 putchar('\n');
 return (0);
