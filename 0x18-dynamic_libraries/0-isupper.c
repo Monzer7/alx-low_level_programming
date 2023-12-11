35172472931234e3fd0973afc38d77c74f8d2cfd
Monzer7 <monzer-7@hotmail.com>
2023-12-11 01:54:41 -0800

    dynamic librarie

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+
+int _isupper (int C)
+{
+int c;
+return _isupper(c);
+}  
+
---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+ 
+int _isdigit(int c)
+{
+return (_isdigit(c));
+}
+
